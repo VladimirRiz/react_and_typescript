931b915d0aaa44c2cbf47795153f3a93c2da4cdf
Vladimir Riz <resin.fhp@gmail.com>
2021-01-15 17:30:36 +0200

    feature: add Article and ArticleList components

---
+++ src/components/ArticleList.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Art from './Article'
+
+interface Article {
+    id: string;
+    date: string;
+    title: string;
+    text: string;
+    comments?: {
+        id: string;
+        user: string;
+        text: string;
+    }[];
+}
+
+interface ArticleListProps {
+    articles: Article[]
+}
+
+const ArticleList = ({ articles }: ArticleListProps) => {
+    const article = articles.map((article) => <li><Art article={article} /></li>)
+    return (
+        <>
+            {article}
+        </>
+    )
+}
+
+export default ArticleList
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { articles } from './fixtures'
+import ArticleList from './components/ArticleList'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+render(<ArticleList articles={articles} />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
